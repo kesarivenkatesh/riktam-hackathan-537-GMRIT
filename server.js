17a5308b340b17e338efd600f1f6210359241b5e
Dhamareshwarakumar <damareshwarakumar@gmail.com>
2021-12-05 19:24:02 +0530

    deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const passport = require('passport');
+const path = requires('path');
 
 
 // Middleware
@@ -33,7 +34,13 @@ app.use('/api', require('./routes/auth'));
 app.use('/api/offer', require('./routes/offer'));
 app.use('/api/request', require('./routes/request'));
 
-
+// Server Static Assess if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // Fireup Server
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
